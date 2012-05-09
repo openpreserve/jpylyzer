c9184777487a89279a2263d8252a32d402d4ae12
Johan van der Knijff <johan.vanderknijff@kb.nl>
2012-05-09 16:02:15 +0200

    Added license info to each module, added jpylyzerUserManual.doc + reference to debian/docs

--- jpylyzer.py
+++ jpylyzer.py
@@ -47,7 +47,7 @@ from byteconv import bytesToText
 from shared import printWarning
 scriptPath, scriptName = os.path.split(sys.argv[0])
 
-__version__= "1.5.0"
+__version__= "1.5.1"
 
 def main_is_frozen():
     return (hasattr(sys, "frozen") or # new py2exe
