058c02a73f89ac987ac50ab89d23d16c8bfd3686
Adam Retter <adam.retter@googlemail.com>
2012-09-21 15:05:25 +0100

    [bugfix] fixed * expansion for file paths so that the shell does the expansion rather than python. Calling 'jpylyzer /some/folder/*' now works

--- jpylyzer.py
+++ jpylyzer.py
@@ -10,7 +10,10 @@
 # Copyright (C) 2011, 2012 Johan van der Knijff, Koninklijke Bibliotheek -
 #  National Library of the Netherlands
 #
-# Contributors: Rene van der Ark (refactoring of original code), Lars Buitinck 
+# Contributors:
+#   Rene van der Ark (refactoring of original code)
+#   Lars Buitinck
+#   Adam Retter, The National Archives, UK. <adam.retter@googlemail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -24,14 +27,6 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# ISSUES:
-# 1. Using wildcards on the command line doesn't result in the expected
-# behaviour under Linux! Workaround: wrap them in quotes, e.g:
-#
-#  jpylyzer.py *  -- only processes 1st encountered file!
-#  jpylyzer.py "*" -- results in correct behaviour
-#
 
 import sys
 import os
@@ -61,14 +56,14 @@ def get_main_dir():
 
 def readFileBytes(file):
     # Read file, return contents as a byte object
-    
+
     # Open file
     f = open(file,"rb")
-    
+
     # Put contents of file into a byte object.
     fileData=f.read()
     f.close()
-    
+
     return(fileData)
 
 def generatePropertiesRemapTable():
@@ -110,7 +105,7 @@ def generatePropertiesRemapTable():
     enumCSMap[16]="sRGB"
     enumCSMap[17]="greyscale"
     enumCSMap[18]="sYCC"
-    
+
     # Profile Class (ICC)
     profileClassMap={}
     profileClassMap[b'scnr']="Input Device Profile"
@@ -120,46 +115,46 @@ def generatePropertiesRemapTable():
     profileClassMap[b'spac']="ColorSpace Conversion Profile"
     profileClassMap[b'abst']="Abstract Profile"
     profileClassMap[b'nmcl']="Named Colour Profile"
-    
+
     # Primary Platform (ICC)
     primaryPlatformMap={}
     primaryPlatformMap[b'APPL']="Apple Computer, Inc."
     primaryPlatformMap[b'MSFT']="Microsoft Corporation"
     primaryPlatformMap[b'SGI']="Silicon Graphics, Inc."
     primaryPlatformMap[b'SUNW']="Sun Microsystems, Inc."
-    
+
     # Transparency (ICC)
     transparencyMap={}
     transparencyMap[0]="Reflective"
     transparencyMap[1]="Transparent"
-    
+
     # Glossiness (ICC)
     glossinessMap={}
     glossinessMap[0]="Glossy"
     glossinessMap[1]="Matte"
-    
+
     # Polarity (ICC)
     polarityMap={}
     polarityMap[0]="Positive"
     polarityMap[1]="Negative"
-    
+
     # Colour (ICC)
     colourMap={}
     colourMap[0]="Colour"
     colourMap[1]="Black and white"
-    
+
     # Rendering intent (ICC)
     renderingIntentMap={}
     renderingIntentMap[0]="Perceptual"
     renderingIntentMap[1]="Media-Relative Colorimetric"
     renderingIntentMap[2]="Saturation"
     renderingIntentMap[3]="ICC-Absolute Colorimetric"
-    
+
     # mTyp (Component Mapping box)
     mTypMap={}
     mTypMap[0]="direct use"
     mTypMap[1]="palette mapping"
-    
+
     # Channel type (Channel Definition Box)
     cTypMap={}
     cTypMap[0]="colour"
@@ -190,13 +185,13 @@ def generatePropertiesRemapTable():
     transformationMap={}
     transformationMap[0]="9-7 irreversible"
     transformationMap[1]="5-3 reversible"
-    
+
     # Quantization style (Codestream, QCD)
     qStyleMap={}
     qStyleMap[0]="no quantization"
     qStyleMap[1]="scalar derived"
     qStyleMap[2]="scalar expounded"
-    
+
     # Registration value (Codestream, COM)
     registrationMap={}
     registrationMap[0]="binary"
@@ -244,24 +239,24 @@ def generatePropertiesRemapTable():
 def checkOneFile(file):
     # Process one file and return analysis result as text string (which contains
     # formatted XML)
-    
+
     fileData = readFileBytes(file)
     isValidJP2, tests, characteristics = BoxValidator("JP2", fileData).validate() #validateJP2(fileData)
-    
+
     # Generate property values remap table
     remapTable = generatePropertiesRemapTable()
 
     # Create printable version of tests and characteristics tree
     tests.makeHumanReadable()
     characteristics.makeHumanReadable(remapTable)
-    
+
     # Create output elementtree object
     root=ET.Element('jpylyzer')
 
     # Create elements for storing tool and file meta info
     toolInfo=ET.Element('toolInfo')
     fileInfo=ET.Element('fileInfo')
-    
+
     # File name and path may contain non-ASCII characters, decoding to Latin should
     # (hopefully) prevent any Unicode decode errors. Elementtree will then deal with any non-ASCII
     # characters by replacing them with numeric entity references
@@ -273,7 +268,7 @@ def checkOneFile(file):
         # This works in Python 3.x, but goes wrong withh non-ASCII chars in 2.7
         fileName=os.path.basename(file)
         filePath=os.path.abspath(file)
-        
+
     # Produce some general tool and file meta info
     toolInfo.appendChildTagWithText("toolName", scriptName)
     toolInfo.appendChildTagWithText("toolVersion", __version__)
@@ -297,30 +292,33 @@ def checkOneFile(file):
     result=root.toxml().decode("ascii")
 
     return(result)
-    
-def checkFiles(images):
-    if len(images) == 0:
+
+def checkFiles(paths):
+    if len(paths) == 0:
         printWarning("no images to check!")
 
-    for image in images:
-            thisFile = image
+    for path in paths:
+            thisPath = path
 
-            isFile = os.path.isfile(thisFile)
+            isFile = os.path.isfile(thisPath)
 
             if isFile:
                 # Analyse file
-                result=checkOneFile(thisFile)
-                
+                result=checkOneFile(thisPath)
+
                 # Write output to stdout
                 sys.stdout.write(result)
+
+            else:
+                isFolder = os.path.isfolder(thisPath)
 
 def parseCommandLine():
     # Create parser
     parser = argparse.ArgumentParser(description="JP2 image validator and properties extractor",version=__version__)
 
     # Add arguments
-    parser.add_argument('jp2In', action="store", help="input JP2 image(s)")
     parser.add_argument('--verbose', action="store_true", dest="outputVerboseFlag", default=False, help="report test results in verbose format")
+    parser.add_argument('jp2In', action="store", nargs=argparse.REMAINDER, help="input JP2 image(s) or folder(s)")
 
     # Parse arguments
     args=parser.parse_args()
@@ -331,16 +329,13 @@ def main():
     # Get input from command line
     args=parseCommandLine()
     jp2In=args.jp2In
-    
+
     # Storing this to 'config.outputVerboseFlag' makes this value available to any module
     # that imports 'config.py' (here: 'boxvalidator.py')
     config.outputVerboseFlag=args.outputVerboseFlag
 
-    # Input images as file list
-    imagesIn=glob.glob(jp2In)
-
-    # Check file
-    checkFiles(imagesIn)
+    # Check files
+    checkFiles(jp2In)
 
 if __name__ == "__main__":
     main()
