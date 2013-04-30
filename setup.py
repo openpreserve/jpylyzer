54d3ff7d438e56467922333bbc9fb4d79df43e0b
Carl Wilson <carl@openplanetsfoundation.org>
2013-04-30 22:15:23 +0200

    Added setup.py to try to use distutils for package build.
    Now an effort to remove PyInstaller dependency as it's a little none standard.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jpylyzer',
+      version='1.9.2',
+      description='Descritpion here',
+      author='Johan van der Knijff',
+      author_email='johan@work',
+      url='http://a place',
+      packages=['jpylyzer'],
+     )
