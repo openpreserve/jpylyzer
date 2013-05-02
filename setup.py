61858269205e95df5f75bc04d8798ac796105bbd
Johan van der Knijff <johan.vanderknijff@kb.nl>
2013-05-02 13:16:57 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='jpylyzer',
-      version='1.9.2',
-      description='Descritpion here',
-      author='Johan van der Knijff',
-      author_email='johan@work',
-      url='http://a place',
-      module=['jpylyzer'],
-     )
+setup(name = 'jpylyzer',
+    version = '1.9.2',
+    description = 'JP2 (JPEG 2000 Part 1) image validator and properties extractor',
+    author = 'Johan van der Knijff',
+    author_email = 'johan.vanderknijff@kb.nl',
+    maintainer = 'Johan van der Knijff',
+    maintainer_email = 'johan.vanderknijff@kb.nl',
+    url = 'https://github.com/openplanets/jpylyzer',
+    packages = ['jpylyzer'],
+    license = 'GNU Lesser General Public License'
+    )
