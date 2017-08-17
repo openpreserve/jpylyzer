557f6f3c4386eb3e2e0a297fe4894970aa999ae1
bitsgalore <johan.vanderknijff@kb.nl>
2017-08-17 15:15:04 +0200

    added script for PyPi build and upload steps

---
+++ package-pypi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script creates a wheel distribution and uploads it to PyPi
+# 
+# Requirements:
+#
+# twine https://pypi.python.org/pypi/twine/1.9.1 (pip install twine)
+# wheel https://pypi.python.org/pypi/wheel (pip install wheel)
+
+# Repository: this is usually pypi; for testing use testpypi
+# The corresponding repository URLS are defined in config file ~/.pypirc
+repository=testpypi
+#repository=pypi
+
+# Clear contents of dist dir
+rm ./dist/*
+
+# Create wheel
+python setup.py sdist bdist_wheel --universal
+
+# Upload package if wheel build was successful; if not show error message
+if [ $? -eq 0 ]; then
+    twine upload --repository $repository  dist/*
+else
+    echo "Wheel build not successful quitting now ..."
+
+fi
+}
+
+
