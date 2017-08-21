6a661028cec96a8c53cd892f3ce2b670a8e71f55
bitsgalore <johan.vanderknijff@kb.nl>
2017-08-21 15:08:14 +0200

    added cli wrapper script to ensure relative imports work correctly in PyInstaller builds

---
+++ jpylyzer/cli.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+#
+"""CLI wrapper script, ensures that relative imports work correctly in a PyInstaller build"""
+from jpylyzer.main import main
+if __name__ == '__main__':
+    main()
+
