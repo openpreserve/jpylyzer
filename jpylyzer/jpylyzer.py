4ac7f45dd88095cadd8b2465ffa08c64d369ea3c
bitsgalore <johan.vanderknijff@kb.nl>
2017-08-21 16:47:53 +0200

    bumped version no

--- jpylyzer/jpylyzer.py
+++ jpylyzer/jpylyzer.py
@@ -63,7 +63,7 @@ scriptPath, scriptName = os.path.split(sys.argv[0])
 if len(scriptName) == 0:
     scriptName = 'jpylyzer'
 
-__version__ = "1.18.0b6"
+__version__ = "1.18.0b7"
 
 # Create parser
 parser = argparse.ArgumentParser(
