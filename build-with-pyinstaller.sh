1cb057819f04334a582b8859f868d1d2b89df244
bitsgalore <johan.vanderknijff@kb.nl>
2017-08-22 13:01:11 +0200

    updated build-with-pyinstaller.sh

--- build-with-pyinstaller.sh
+++ build-with-pyinstaller.sh
@@ -2,9 +2,12 @@
 
 # Bash script to build jpylyzer using PyInstaller
 
+# Script base name (i.e. script name minus .py extension)
+scriptBaseName=jpylyzer
+
 # First check for PyInstaller
 command -v pyinstaller >/dev/null 2>&1 || {
-    echo >&2 "PyInstaller is required to build the Jpylyzer executable.";
+    echo >&2 "PyInstaller is required to build the executable.";
     echo >&2 "Please install PyInstaller with:";
     echo >&2 "  (sudo) pip install pyinstaller"
     exit 1;
@@ -17,12 +20,12 @@ userId=$originalUserId
 if [ $originalUserId == 0 ]
 then
     uname=$(getent passwd 1000 | cut -d: -f1)
-    sudo -u $uname "pyi-makespec --strip --onefile --paths=jpylyzer --specpath=pyi-build ./jpylyzer/jpylyzer.py"
-    sudo -u $uname "pyinstaller --strip --clean --paths=jpylyzer --distpath=pyi-build/dist --workpath=pyi-build/build ./pyi-build/jpylyzer.spec"
+    sudo -u $uname "pyi-makespec --strip --onefile --paths=$scriptBaseName --name=$scriptBaseName --specpath=pyi-build ./cli.py"
+    sudo -u $uname "pyinstaller --strip --clean --paths=$scriptBaseName --distpath=pyi-build/dist --workpath=pyi-build/build ./pyi-build/$scriptBaseName.spec"
 else
     # So making stripped binaries for debian packaging
-    pyi-makespec --strip --onefile --paths=jpylyzer --specpath=pyi-build ./jpylyzer/jpylyzer.py
-    pyinstaller --strip --clean --paths=jpylyzer --distpath=pyi-build/dist --workpath=pyi-build/build ./pyi-build/jpylyzer.spec
+    pyi-makespec --strip --onefile --paths=$scriptBaseName --name=$scriptBaseName --specpath=pyi-build ./cli.py
+    pyinstaller --strip --clean --paths=$scriptBaseName --distpath=pyi-build/dist --workpath=pyi-build/build ./pyi-build/$scriptBaseName.spec
 fi
 
-./pyi-build/dist/jpylyzer --version;
+./pyi-build/dist/$scriptBaseName --version;
