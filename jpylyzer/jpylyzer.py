74f4e65780e37f44b269c163ec93b39e5d1c6502
bitsgalore <johan.vanderknijff@kb.nl>
2019-10-29 12:19:21 +0100

    bumped version number to 2.0.0rc1

--- jpylyzer/jpylyzer.py
+++ jpylyzer/jpylyzer.py
@@ -56,7 +56,7 @@ SCRIPT_PATH, SCRIPT_NAME = os.path.split(sys.argv[0])
 if not SCRIPT_NAME:
     SCRIPT_NAME = 'jpylyzer'
 
-__version__ = "2.0.0a1"
+__version__ = "2.0.0rc1"
 
 # Create PARSER
 PARSER = argparse.ArgumentParser(
