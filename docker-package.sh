0ec7cdee20672bbc7d4700dc7399da4adf975256
bitsgalore <johan.vanderknijff@kb.nl>
2019-11-18 18:35:33 +0100

    automatically derive Debian version string from python version string

--- docker-package.sh
+++ docker-package.sh
@@ -11,8 +11,33 @@ dist_id=${image%%:*}
 codename=${image#*:}
 pypi_name="$(./setup.py --name)"
 pypi_version="$(./setup.py --version)"
-#deb_version=2.0.0~rc1
-deb_version=$pypi_version
+
+# Derive Debian version string from pypi_name
+vMajor="$(cut -d'.' -f1 <<<$pypi_version)"
+vMinor="$(cut -d'.' -f2 <<<$pypi_version)"
+vDebug="$(cut -d'.' -f3 <<<$pypi_version)"
+# Length of debug string
+lDebug=${#vDebug} 
+fixVDebugFlag=False
+re='^[0-9]+$'
+for i in $(seq 1 ${#vDebug}); do
+    echo $i
+    char="${vDebug:i-1:1}"
+    if [[ $char =~ $re ]] ; then
+        splitpos=$i
+    else
+        fixVDebugFlag=True
+        break
+    fi
+done
+
+if  [ $fixVDebugFlag = "True" ] ; then 
+    vDebugDebian=${vDebug:0:$splitpos}"~"${vDebug:$splitpos:$lDebug}
+else
+    vDebugDebian=$vDebug
+fi
+deb_version="$vMajor.$vMinor.$vDebugDebian"
+
 pkgname=$pypi_name
 tag=$pypi_name-$dist_id-$codename
 # Build in Docker container, save results, and show package info
