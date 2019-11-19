ad852eece00ec137a935e339c2db12d28c1d1532
bitsgalore <johan.vanderknijff@kb.nl>
2019-11-19 12:42:39 +0100

    automatically derive Debian version string from Python string, now refactored into function

--- docker-package.sh
+++ docker-package.sh
@@ -2,6 +2,46 @@
 #
 # Build Debian package in a Docker container
 #
+function getDebianVersionString {
+    # Set Debian version string derived from
+    # Python version string
+    pyVersion=$1
+
+    # Split into major, minor and debug version
+    vMajor="$(cut -d'.' -f1 <<<$pyVersion)"
+    vMinor="$(cut -d'.' -f2 <<<$pyVersion)"
+    vDebug="$(cut -d'.' -f3 <<<$pyVersion)"
+
+    # Length of debug string
+    lDebug=${#vDebug}
+
+    # Flag that is True if debug string needs fixing 
+    fixVDebugFlag=False
+
+    # Locate position of 1st non-numeric character
+    # in debug string
+    re='^[0-9]+$'
+    for i in $(seq 1 ${#vDebug}); do
+        echo $i
+        char="${vDebug:i-1:1}"
+        if [[ $char =~ $re ]] ; then
+            splitpos=$i
+        else
+            fixVDebugFlag=True
+            break
+        fi
+    done
+
+    # Insert tilde in debug string if needed
+    if  [ $fixVDebugFlag = "True" ] ; then 
+        vDebugDebian=${vDebug:0:$splitpos}"~"${vDebug:$splitpos:$lDebug}
+    else
+        vDebugDebian=$vDebug
+    fi
+
+    # Construct Debian version string
+    deb_version="$vMajor.$vMinor.$vDebugDebian"
+    }
 
 set -e
 
@@ -11,33 +51,8 @@ dist_id=${image%%:*}
 codename=${image#*:}
 pypi_name="$(./setup.py --name)"
 pypi_version="$(./setup.py --version)"
-
-# Derive Debian version string from pypi_name
-vMajor="$(cut -d'.' -f1 <<<$pypi_version)"
-vMinor="$(cut -d'.' -f2 <<<$pypi_version)"
-vDebug="$(cut -d'.' -f3 <<<$pypi_version)"
-# Length of debug string
-lDebug=${#vDebug} 
-fixVDebugFlag=False
-re='^[0-9]+$'
-for i in $(seq 1 ${#vDebug}); do
-    echo $i
-    char="${vDebug:i-1:1}"
-    if [[ $char =~ $re ]] ; then
-        splitpos=$i
-    else
-        fixVDebugFlag=True
-        break
-    fi
-done
-
-if  [ $fixVDebugFlag = "True" ] ; then 
-    vDebugDebian=${vDebug:0:$splitpos}"~"${vDebug:$splitpos:$lDebug}
-else
-    vDebugDebian=$vDebug
-fi
-deb_version="$vMajor.$vMinor.$vDebugDebian"
-
+# Generate Debian version string
+getDebianVersionString $pypi_version
 pkgname=$pypi_name
 tag=$pypi_name-$dist_id-$codename
 # Build in Docker container, save results, and show package info
