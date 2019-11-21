5aea5e99eb3f4aec07483c6271caa468945d1b85
Carl Wilson <carl@openpreservation.org>
2019-11-21 04:39:14 +0000

    FIX - unused variable in bash script.

--- docker-package-pypi.sh
+++ docker-package-pypi.sh
@@ -5,8 +5,5 @@
 
 set -e
 
-# Get build platform as 1st argument, and collect project metadata
-pypi_name="$(./setup.py --name)"
-
 # Build in Docker container, save results, and show package info
-docker run -v "$PWD":/src -v "$HOME":/pypirc --entrypoint "/src/package-pypi.sh" --rm python:3.6-stretch 
+docker run -v "$PWD":/src -v "$HOME":/pypirc --entrypoint "/src/package-pypi.sh" --rm python:3.6-stretch
