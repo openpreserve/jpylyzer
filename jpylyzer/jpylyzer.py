159275f847dfa87d9486773f53a3d40585bf8edf
Tim Lander <tim57282@hotmail.com>
2020-06-26 13:43:31 +0800

    Fix indentation to pass linter

--- jpylyzer/jpylyzer.py
+++ jpylyzer/jpylyzer.py
@@ -279,8 +279,8 @@ def fileToMemoryMap(filename):
             fileSize = os.path.getsize(filename)
             if fileSize > sys.maxsize:
                 shared.printWarning("The file is too large to open (" +
-                             str(round(fileSize/1024**3, 1)) +
-                             " GB). Try using 64-bit python.")
+                                    str(round(fileSize/1024**3, 1)) +
+                                    " GB). Try using 64-bit python.")
             shared.errorExit("Opening file failed: " + str(e))
             # mmap fails on empty files.
             fileData = ""
