93af96ee093b68be002bf9de87428d727688e166
bitsgalore <johan.vanderknijff@kb.nl>
2022-01-17 19:27:51 +0100

    added remaining tests files

--- tests/unit/test_testfiles.py
+++ tests/unit/test_testfiles.py
@@ -24,16 +24,73 @@ testFiles = {
 "meth_is_2_no_icc.jp2": "False",
 "height_image_header_damaged.jp2": "False",
 "triggerUnboundLocalError.jp2": "False",
-"modified_date_out_of_range.jp2": "True"
+"modified_date_out_of_range.jp2": "True",
+"ランダム日本語テキスト.jp2": "True",
+"隨機中國文字.jp2": "True",
+"is_codestream.jp2": "False",
+"is_jpx.jp2": "False",
+"is_jpm.jp2": "False",
+"is_jpeg.jp2": "False",
+"jpx_disguised_as_jp2.jp2": "False",
+"kakadu61.jp2": "True",
+"kakadu71.jp2": "True",
+"aware.jp2": "True",
+"openJPEG15.jp2": "True",
+"graphicsMagick.jp2": "True",
+"oj-illegal-rcom-value.jp2": "False",
+"oj-tnsot-0.jp2": "True",
+"oj-ytsiz-not-valid-1.jp2": "False",
+"oj-ytsiz-not-valid-2.jp2": "False",
+"oj-xtsiz-not-valid-1.jp2": "False",
+"oj-issue363-4740.jp2": "False",
+"oj-poc-main-header.jp2": "True",
+"oj-rgn-main-header-1.jp2": "True",
+"oj-rgn-tilepart-header-1.jp2": "True",
+"oj-plm-main-header.jp2": "False",
+"oj-ppm-main-header-1.jp2": "False",
+"oj-ppm-main-header-2.jp2": "False",
+"oj-ppm-main-header-3.jp2": "False",
+"oj-ppt-tilepart-header.jp2": "False",
+"tika-tlm-main-header.jp2": "True",
+"tika-crg-main-header.jp2": "True",
+"null_terminated_content_in_xml_box.jp2": "False",
+"palettedImage.jp2": "True",
+"sentinel.jp2": "True",
+"bitwiser-codestreamheader-corrupted-xsiz-10918.jp2": "False",
+"bitwiser-codestreamheader-corrupted-xsiz-10928.jp2": "False",
+"bitwiser-codestreamheader-corrupted-xsiz-10937.jp2": "False",
+"bitwiser-codestreamheader-corrupted-xsiz-10946.jp2": "False",
+"bitwiser-codestreamheader-corrupted-xsiz-10955.jp2": "False",
+"bitwiser-codestreamheader-corrupted-ysiz-11208.jp2": "False",
+"bitwiser-codestreamheader-corrupted-ysiz-11218.jp2": "False",
+"bitwiser-codestreamheader-corrupted-ysiz-11227.jp2": "False",
+"bitwiser-codestreamheader-corrupted-ysiz-11238.jp2": "False",
+"bitwiser-codestreamheader-corrupted-ysiz-11252.jp2": "False",
+"bitwiser-headerbox-corrupted-boxlength-22181.jp2": "False",
+"bitwiser-icc-corrupted-tagcount-1911.jp2": "True",
+"bitwiser-icc-corrupted-tagcount-1920.jp2": "True",
+"bitwiser-icc-corrupted-tagcount-1937.jp2": "True",
+"bitwiser-icc-corrupted-tagcount-1951.jp2": "True",
+"bitwiser-icc-corrupted-tagcount-1961.jp2": "True",
+"bitwiser-icc-corrupted-tagcount-1971.jp2": "True",
+"bitwiser-icc-corrupted-tagcount-1984.jp2": "True",
+"bitwiser-icc-corrupted-tagcount-1999.jp2": "True",
+"bitwiser-icc-corrupted-tagcount-2011.jp2": "True",
+"bitwiser-icc-corrupted-tagcount-2021.jp2": "True",
+"bitwiser-resolutionbox-corrupted-boxlength-8127.jp2": "False",
+"bitwiser-resolutionbox-corrupted-boxlength-8154.jp2": "False",
+"bitwiser-resolutionbox-corrupted-boxlength-8730.jp2": "False",
+"oj-tileindex-error-1.jp2": "False",
+"oj-tileindex-error-2.jp2": "False",
+"oj-tileindex-error-3.jp2": "False",
+"oj-tileindex-error-4.jp2": "False",
+"oj-tileindex-error-5.jp2": "False"
 }
 
 ## Excluded in above dict are:
 #
-# bitwiser-icc-corrupted-tagcount*.jp2: all True
-# bitwiser-resolutionbox*.jp2: all False
-# bitwiser-headerbox*.jp2: all False
-# bitwiser-codestreamheader*.jp2: all False
-#
+# - 3 surrogate pair samples: needs separate test
+# - empty.jp2: needs separate test
 
 def fileIsValid(testFile):
     VALIDATION_FORMAT = 'jp2'
