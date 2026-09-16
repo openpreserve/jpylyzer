710423ec6df670a9dc9d2e8cd7c53bf4bf1e0b72
bitsgalore <johan.vanderknijff@kb.nl>
2026-09-16 19:50:35 +0200

    updated docstring

--- jpylyzer/boxvalidator.py
+++ jpylyzer/boxvalidator.py
@@ -1,4 +1,4 @@
-"""Validator class for all boxes in JP2."""
+"""Validator class for all boxes in JP2 and JPH"""
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
